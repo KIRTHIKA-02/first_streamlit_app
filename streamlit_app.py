e8a90f41d55d5bf47e2af098f6c140251fc340b3
KIRTHIKA-02 <137144511+KIRTHIKA-02@users.noreply.github.com>
2023-06-20 15:44:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.header('Breakfast Favourites')
 
 
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal ')
-streamlit.text('🥗Kale , Spinach & Rocket Smoothie')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free Range Egg')
 streamlit.text('🥑🍞Avacoda Toast')
 
